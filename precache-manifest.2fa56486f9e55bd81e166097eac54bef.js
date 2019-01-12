be2480918c97f045c46328a2e3da07370c4da3cd
ashutoshjangra <ashutoshjangra603@gmail.com>
2019-01-12 22:54:56 +0530

    Updates

---
+++ precache-manifest.2fa56486f9e55bd81e166097eac54bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3068285a94e79392a7d0",
+    "url": "/quiz/static/js/runtime~main.3068285a.js"
+  },
+  {
+    "revision": "de28f1c80e595c128350",
+    "url": "/quiz/static/js/main.de28f1c8.chunk.js"
+  },
+  {
+    "revision": "29c899d54be9b216b439",
+    "url": "/quiz/static/js/1.29c899d5.chunk.js"
+  },
+  {
+    "revision": "de28f1c80e595c128350",
+    "url": "/quiz/static/css/main.79e3dd74.chunk.css"
+  },
+  {
+    "revision": "0d774d8c6a375a6ce9b15ffb8eec1fed",
+    "url": "/quiz/index.html"
+  }
+];
